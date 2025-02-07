2e3ff2d738c3abf9bfe5feeb0df08e8e511277a6
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-02-06 20:04:35 -0600

    Adding more dirt/mud

--- kubejs/server_scripts/@recipes/create/dirt.js
+++ kubejs/server_scripts/@recipes/create/dirt.js
@@ -1,6 +1,16 @@
 ServerEvents.recipes((x) => {
   x.recipes.create.mixing(
-    '3x minecraft:dirt',
+    '2x minecraft:mud',
     [Fluid.water(1000), 'minecraft:gravel', 'minecraft:sand']
-  )
+  );
+
+  x.recipes.create.mixing('minecraft:dirt', 'minecraft:mud')
+    .heatRequirement('heated')
+    .processingTime(300)
+
+  x.replaceOutput(
+    {type: 'minecraft:smoking', input: 'minecraft:mud'},
+    'minecraft:clay',
+    'minecraft:dirt'
+  );
 })
