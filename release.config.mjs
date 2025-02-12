e987da101a8eb0ff204d5170384ce3823d2083c0
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-02-11 21:45:36 -0600

    fix(chore): Fixing semantic release

--- release.config.mjs
+++ release.config.mjs
@@ -7,6 +7,12 @@ export default {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/github',
-    '@semantic-release/git',
+    [
+      '@semantic-release/git',
+      {
+        message:
+          'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+      },
+    ],
   ],
 };
