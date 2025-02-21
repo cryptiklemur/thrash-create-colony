35843229364610c25e4c2ec430225e4a63cef3f8
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-02-21 03:05:24 -0600

    fix(recipe): Adding crushing recipe for rose quartz block

--- kubejs/server_scripts/@recipes/create/miscellaneous.js
+++ kubejs/server_scripts/@recipes/create/miscellaneous.js
@@ -6,6 +6,12 @@ ServerEvents.recipes((x) => {
     '#forge:ingots/copper',
   ]);
 
+  x.recipes.create.crushing([
+    Item.of('biomesoplenty:rose_quartz_chunk', 4),
+    Item.of('biomesoplenty:rose_quartz_chunk', 4).withChance(0.75),
+    Item.of('biomesoplenty:rose_quartz_chunk', 4).withChance(0.25),
+  ]);
+
   x.custom({
     type: 'create_new_age:energising',
     energy_needed: 10000,
